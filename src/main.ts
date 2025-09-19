070b1a12d81907fcdb716a67af4d553ad88b57f5
unknown <nathaliav8194@gmail.com>
2025-09-19 18:56:22 -0500

    Linker

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
+import { Homepage } from './app/features/homepage/homepage';
 
-bootstrapApplication(App, appConfig)
+bootstrapApplication(Homepage, appConfig)
   .catch((err) => console.error(err));
